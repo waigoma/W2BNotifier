7b175ab5da0de7fd26b901393a22998c0b4c6b01
waigoma <waichi0302@gmail.com>
2022-02-03 23:54:24 +0900

    initial commit

---
+++ src/main/kotlin/com/waigoma/w2bnotifier/W2BNotifierApplication.kt
@@ -0,0 +1,11 @@
+package com.waigoma.w2bnotifier
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W2BNotifierApplication
+
+fun main(args: Array<String>) {
+    runApplication<W2BNotifierApplication>(*args)
+}
---
+++ src/test/kotlin/com/waigoma/w2bnotifier/W2BNotifierApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.waigoma.w2bnotifier
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W2BNotifierApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
