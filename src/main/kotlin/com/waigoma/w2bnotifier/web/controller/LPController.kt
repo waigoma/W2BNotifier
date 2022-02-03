90c74d4ad3520a337a3e20236220bc37f70a138a
waigoma <waichi0302@gmail.com>
2022-02-04 00:05:32 +0900

    update LPController

--- src/main/kotlin/com/waigoma/w2bnotifier/web/controller/LPController.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/web/controller/LPController.kt
@@ -1,4 +1,12 @@
 package com.waigoma.w2bnotifier.web.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class LPController {
+    @GetMapping("/")
+    fun index(): String {
+        return "index"
+    }
 }
