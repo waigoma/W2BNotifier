8191822d82eea351b462dd534c4f2bba5801cab9
waigoma <waichi0302@gmail.com>
2022-02-04 19:16:21 +0900

    update YamlLoader

--- src/main/kotlin/com/waigoma/w2bnotifier/file/YamlLoader.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/file/YamlLoader.kt
@@ -1,4 +1,19 @@
 package com.waigoma.w2bnotifier.file
 
+import org.yaml.snakeyaml.Yaml
+import java.io.File
+import java.io.InputStreamReader
+
 class YamlLoader {
+    fun load(file: String): Map<String, BotYamlTemplate> {
+        val reader = InputStreamReader(File(file).inputStream())
+
+        val yaml = Yaml()
+        val valueMap = yaml.load<HashMap<String, String>>(reader)
+
+        val botYamlMap = HashMap<String, BotYamlTemplate>()
+        botYamlMap["discord"] = BotYamlTemplate(valueMap["discord-token"]!!, valueMap["discord-room"]!!)
+
+        return botYamlMap
+    }
 }
