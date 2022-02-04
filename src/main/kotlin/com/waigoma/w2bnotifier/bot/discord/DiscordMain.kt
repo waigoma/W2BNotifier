fe91e64d67795a02492fc8cf67daad6d437f1e0d
waigoma <waichi0302@gmail.com>
2022-02-04 18:28:34 +0900

    add DiscordMain

---
+++ src/main/kotlin/com/waigoma/w2bnotifier/bot/discord/DiscordMain.kt
@@ -0,0 +1,11 @@
+package com.waigoma.w2bnotifier.bot.discord
+
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.events.ReadyEvent
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+
+class DiscordMain : ListenerAdapter() {
+
+}
