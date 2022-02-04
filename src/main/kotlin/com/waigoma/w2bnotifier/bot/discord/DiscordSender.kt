5ff296a17c8d484e7b2b62454dde82fbe14c8276
waigoma <waichi0302@gmail.com>
2022-02-04 21:53:46 +0900

    update DiscordSender

--- src/main/kotlin/com/waigoma/w2bnotifier/bot/discord/DiscordSender.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/bot/discord/DiscordSender.kt
@@ -1,10 +1,9 @@
 package com.waigoma.w2bnotifier.bot.discord
 
-class DiscordSender {
-    private val jda = DiscordMain.jda
+import net.dv8tion.jda.api.JDA
 
+class DiscordSender(private val jda: JDA) {
     fun sendText(text: String, roomId: Long) {
         jda.getTextChannelById(roomId)?.sendMessage(text)!!.queue()
     }
-
 }
