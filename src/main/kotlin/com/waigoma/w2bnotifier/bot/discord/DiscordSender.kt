54012ff40417cafc35baae55b1572d9771122220
waigoma <waichi0302@gmail.com>
2022-02-05 18:09:38 +0900

    rename variable

--- src/main/kotlin/com/waigoma/w2bnotifier/bot/discord/DiscordSender.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/bot/discord/DiscordSender.kt
@@ -3,7 +3,7 @@ package com.waigoma.w2bnotifier.bot.discord
 import net.dv8tion.jda.api.JDA
 
 class DiscordSender(private val jda: JDA) {
-    fun sendText(text: String, roomId: Long) {
-        jda.getTextChannelById(roomId)?.sendMessage(text)!!.queue()
+    fun sendText(text: String, channelId: Long) {
+        jda.getTextChannelById(channelId)?.sendMessage(text)!!.queue()
     }
 }
