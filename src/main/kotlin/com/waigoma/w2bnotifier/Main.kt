3450c63bb7305f1367cfec23d089edd5e408a169
waigoma <waichi0302@gmail.com>
2022-02-05 14:54:11 +0900

    update Main

--- src/main/kotlin/com/waigoma/w2bnotifier/Main.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/Main.kt
@@ -1,19 +1,26 @@
 package com.waigoma.w2bnotifier
 
+import com.waigoma.w2bnotifier.bot.discord.DiscordMain
 import com.waigoma.w2bnotifier.file.ResourceExporter
 import com.waigoma.w2bnotifier.file.YamlManager
 
 class Main {
     companion object {
         private lateinit var yamlManager: YamlManager
+        private lateinit var discordMain: DiscordMain
 
         fun getYamlManager(): YamlManager {
             return yamlManager
+        }
+
+        fun getDiscordMain(): DiscordMain {
+            return discordMain
         }
     }
 
     init {
         ResourceExporter().export()
         yamlManager = YamlManager()
+        discordMain = DiscordMain()
     }
 }
