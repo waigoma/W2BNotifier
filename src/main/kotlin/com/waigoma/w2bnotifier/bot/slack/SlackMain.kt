88a4e82c7a38f281040d20228493890a6d03559a
waigoma <waichi0302@gmail.com>
2022-02-05 21:53:24 +0900

    update SlackMain

--- src/main/kotlin/com/waigoma/w2bnotifier/bot/slack/SlackMain.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/bot/slack/SlackMain.kt
@@ -1,4 +1,18 @@
 package com.waigoma.w2bnotifier.bot.slack
 
+import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory
+import com.waigoma.w2bnotifier.Main
+
 class SlackMain {
+    private val logger = org.slf4j.LoggerFactory.getLogger(this::class.java)
+
+    private val yml = Main.getYamlManager()
+    private val slackSession = SlackSessionFactory.createWebSocketSlackSession(yml.getSlackData().TOKEN)
+
+    fun sendMessage(message: String) {
+        slackSession.connect()
+        logger.info("Slack session connected")
+        slackSession.sendMessage(yml.getSlackData().CHANNEL, message)
+        slackSession.disconnect()
+    }
 }
