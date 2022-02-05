a0630d988fb7947dbd40f7204c1069ddc1af032c
waigoma <waichi0302@gmail.com>
2022-02-05 21:46:03 +0900

    add Slack

--- src/main/kotlin/com/waigoma/w2bnotifier/Main.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/Main.kt
@@ -1,6 +1,7 @@
 package com.waigoma.w2bnotifier
 
 import com.waigoma.w2bnotifier.bot.discord.DiscordMain
+import com.waigoma.w2bnotifier.bot.slack.SlackMain
 import com.waigoma.w2bnotifier.file.ResourceExporter
 import com.waigoma.w2bnotifier.file.YamlManager
 
@@ -8,6 +9,7 @@ class Main {
     companion object {
         private lateinit var yamlManager: YamlManager
         private lateinit var discordMain: DiscordMain
+        private lateinit var slackMain: SlackMain
 
         fun getYamlManager(): YamlManager {
             return yamlManager
@@ -16,11 +18,16 @@ class Main {
         fun getDiscordMain(): DiscordMain {
             return discordMain
         }
+
+        fun getSlackMain(): SlackMain {
+            return slackMain
+        }
     }
 
     init {
         ResourceExporter().export()
         yamlManager = YamlManager()
         discordMain = DiscordMain()
+        slackMain = SlackMain()
     }
 }
--- src/main/kotlin/com/waigoma/w2bnotifier/file/YamlManager.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/file/YamlManager.kt
@@ -4,13 +4,19 @@ import java.io.File
 
 class YamlManager {
     private val discordData: BotYamlTemplate
+    private val slackData: BotYamlTemplate
 
     init {
         val dataMap = YamlLoader().load(ResourceExporter.DIR_PATH + File.separator + "bot.yml")
         discordData = dataMap["discord"]!!
+        slackData = dataMap["slack"]!!
     }
 
     fun getDiscordData(): BotYamlTemplate {
         return discordData
+    }
+
+    fun getSlackData(): BotYamlTemplate {
+        return slackData
     }
 }
