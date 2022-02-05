55a6b8649ded740a4782f23bb68a4fee5ed04095
waigoma <waichi0302@gmail.com>
2022-02-05 18:23:39 +0900

    add post

--- src/main/kotlin/com/waigoma/w2bnotifier/web/controller/LPController.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/web/controller/LPController.kt
@@ -1,12 +1,24 @@
 package com.waigoma.w2bnotifier.web.controller
 
+import com.waigoma.w2bnotifier.Main
+import com.waigoma.w2bnotifier.bot.discord.DiscordSender
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class LPController {
     @GetMapping("/")
     fun index(): String {
+        return "index"
+    }
+
+    @PostMapping("/")
+    fun post(@RequestParam("discord_msg") discordMsg: String): String {
+        if (discordMsg.isNotEmpty()) {
+            DiscordSender(Main.getDiscordMain().getJDA()).sendText(discordMsg, Main.getYamlManager().getDiscordData().CHANNEL)
+        }
         return "index"
     }
 }
