da3956f8cb1ebc6a46a143e3e8df760a458f752b
waigoma <waichi0302@gmail.com>
2022-02-06 21:40:05 +0900

    update LineMain

--- src/main/kotlin/com/waigoma/w2bnotifier/bot/line/LineMain.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/bot/line/LineMain.kt
@@ -1,4 +1,41 @@
 package com.waigoma.w2bnotifier.bot.line
 
+import com.waigoma.w2bnotifier.Main
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
+import java.util.stream.Collectors
+
+
 class LineMain {
+    private val yml = Main.getYamlManager()
+    private val token = yml.getLineData().TOKEN
+
+    fun sendMessage(message: String) {
+        var connection: HttpURLConnection? = null
+        try {
+            val url = URL("https://notify-api.line.me/api/notify")
+            connection = url.openConnection() as HttpURLConnection
+            connection.doOutput = true
+            connection.requestMethod = "POST"
+            connection.addRequestProperty("Authorization", "Bearer $token")
+            connection.outputStream.use { os ->
+                PrintWriter(os).use { writer ->
+                    writer.append("message=").append(URLEncoder.encode(message, "UTF-8")).flush()
+                    connection.inputStream.use { `is` ->
+                        BufferedReader(InputStreamReader(`is`)).use { r ->
+                            val res = r.lines().collect(Collectors.joining())
+                            if (!res.contains("\"message\":\"ok\"")) {
+                                println(res)
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (ignore: Exception) {
+        } finally {
+            connection?.disconnect()
+        }
+    }
 }
