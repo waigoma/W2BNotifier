9e8a6c1ae260fa6d9083ed3acf48008ab81d6a15
waigoma <waichi0302@gmail.com>
2022-02-07 15:34:12 +0900

    update

--- src/main/kotlin/com/waigoma/w2bnotifier/bot/line/LineMain.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/bot/line/LineMain.kt
@@ -1,41 +1,22 @@
 package com.waigoma.w2bnotifier.bot.line
 
+import com.linecorp.bot.client.LineMessagingClient
+import com.linecorp.bot.model.PushMessage
+import com.linecorp.bot.model.message.TextMessage
 import com.waigoma.w2bnotifier.Main
-import java.io.*
-import java.net.HttpURLConnection
-import java.net.URL
-import java.net.URLEncoder
-import java.util.stream.Collectors
-
 
 class LineMain {
+    private val logger = org.slf4j.LoggerFactory.getLogger(this::class.java)
     private val yml = Main.getYamlManager()
-    private val token = yml.getLineData().TOKEN
+
+    private val client = LineMessagingClient.builder(yml.getLineData().TOKEN).build()
 
     fun sendMessage(message: String) {
-        var connection: HttpURLConnection? = null
-        try {
-            val url = URL("https://notify-api.line.me/api/notify")
-            connection = url.openConnection() as HttpURLConnection
-            connection.doOutput = true
-            connection.requestMethod = "POST"
-            connection.addRequestProperty("Authorization", "Bearer $token")
-            connection.outputStream.use { os ->
-                PrintWriter(os).use { writer ->
-                    writer.append("message=").append(URLEncoder.encode(message, "UTF-8")).flush()
-                    connection.inputStream.use { `is` ->
-                        BufferedReader(InputStreamReader(`is`)).use { r ->
-                            val res = r.lines().collect(Collectors.joining())
-                            if (!res.contains("\"message\":\"ok\"")) {
-                                println(res)
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (ignore: Exception) {
-        } finally {
-            connection?.disconnect()
-        }
+        val textMessage = TextMessage(message)
+        val pushMessage = PushMessage(yml.getLineData().CHANNEL, textMessage)
+
+        val botApiResponse = client.pushMessage(pushMessage).get()
+
+        logger.info("botApiResponse: $botApiResponse")
     }
 }
