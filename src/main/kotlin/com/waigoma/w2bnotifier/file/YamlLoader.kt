ea722677ef0a022bdc8e1f171264b6acf60f8cec
waigoma <waichi0302@gmail.com>
2022-02-07 14:22:54 +0900

    update line

--- src/main/kotlin/com/waigoma/w2bnotifier/file/YamlLoader.kt
+++ src/main/kotlin/com/waigoma/w2bnotifier/file/YamlLoader.kt
@@ -17,13 +17,14 @@ class YamlLoader {
         val discordChannel = data["discord-channel"]!!.toString()
 
         val slackToken = data["slack-token"]!!.toString()
-        val slackChannelStr = data["slack-channel"]!!.toString()
+        val slackChannel = data["slack-channel"]!!.toString()
 
         val lineToken = data["line-token"]!!.toString()
+        val lineChannel = data["line-channel"]!!.toString()
 
         botYamlMap["discord"] = BotYamlTemplate(discordToken, discordChannel)
-        botYamlMap["slack"] = BotYamlTemplate(slackToken, slackChannelStr)
-        botYamlMap["line"] = BotYamlTemplate(lineToken, "none")
+        botYamlMap["slack"] = BotYamlTemplate(slackToken, slackChannel)
+        botYamlMap["line"] = BotYamlTemplate(lineToken, lineChannel)
 
         return botYamlMap
     }
